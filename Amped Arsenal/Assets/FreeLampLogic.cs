11b65bf349c4707adda039893cb85cf7d6c87161
Jacob Boyce <boysee4545@gmail.com>
2022-12-13 03:13:02 -0700

    Added FreeLamp
    Added the lamp then when in proximity a countdown starts and then will give you a free artifact after you stay in it till the timer ends

---
+++ Amped Arsenal/Assets/FreeLampLogic.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FreeLampLogic : MonoBehaviour
+{
+    public TextMeshProUGUI countdownText;
+    public Light lampLight;
+    private string formatTime;
+    public float countdown, cdMax;
+    public bool inRange, done = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = cdMax;
+        formatTime = countdown.ToString("0");
+        countdownText.text = formatTime;
+        lampLight.intensity = 14;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(inRange && done == false)
+        {
+            countdown -= Time.deltaTime;
+            formatTime = countdown.ToString("0");
+            countdownText.text = formatTime;
+            if (countdown <= 0)
+            {
+                lampLight.intensity = 0;
+                done = true;
+                countdownText.text = "";
+                //spawn artifact
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+           inRange = true;
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inRange = false;
+        }
+    }
+
+}
